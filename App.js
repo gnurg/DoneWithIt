3330c6b457b0bd834471c34a06236d1f7a126476
riccardo <17333864+gnurg@users.noreply.github.com>
2021-02-06 23:29:46 +0000

    Added vscode launch

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  let x=1;
+  console.log("App started");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello React Native! Open up App.js!</Text>
       <StatusBar style="auto" />
     </View>
   );
